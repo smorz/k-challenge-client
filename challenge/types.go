6c5e080c1b954f9bffab5265d0d1eecef1e4f35e
smorz <razawian@gmail.com>
2022-02-02 07:50:18 +0330

    Add Copyable type

--- challenge/types.go
+++ challenge/types.go
@@ -2,6 +2,12 @@ package challenge
 
 import "database/sql"
 
+type Copyable interface {
+	table() string
+	fields() []interface{}
+	values() ([]interface{}, bool)
+}
+
 type Generator struct {
 	db *sql.DB
 }
